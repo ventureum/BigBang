26e9e344bc281fe012655f03a7bcd5ccb7565e90
davidhhshao <h3shao@uwaterloo.ca>
2018-10-10 10:44:09 -0700

    add foreign key reference

--- internal/platform/postgres_config/TCR/objective_config/objective_schema.go
+++ internal/platform/postgres_config/TCR/objective_config/objective_schema.go
@@ -13,7 +13,8 @@ CREATE TABLE objectives (
     total_weight BIGINT NOT NULL DEFAULT 0,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    PRIMARY KEY (project_id, milestone_id, objective_id)
+    PRIMARY KEY (project_id, milestone_id, objective_id),
+    FOREIGN KEY (project_id, milestone_id) REFERENCES milestones (project_id, milestone_id)
 );
 CREATE INDEX objectives_index ON objectives (project_id, milestone_id, objective_id);
 CREATE INDEX objectives_asc_index ON objectives (objective_id ASC NULLS LAST);
